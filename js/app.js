4aa926d1b24dc3d2f2417a34fb8413e7eb8e90c7
AbolfazlPanahiazar <55955710+AbolfazlPanahiazar@users.noreply.github.com>
2020-05-04 05:54:15 +0430

    modify style and add roll dice button scripts

--- js/app.js
+++ js/app.js
@@ -3,8 +3,35 @@
 */
 
 let scores = [0, 0];
-let roundSnore = 0;
+let roundScore = 0;
 let activePlayer = 0;
+let diceNumber;
 
-let dice = Math.floor(Math.random() * 6) + 1;
+// Hide the dice square in the first place.
+document.getElementById('dice').style.display = 'none';
 
+// Set values to zero in the first place
+document.querySelector('#score-0').textContent = '0';
+document.querySelector('#score-1').textContent = '0';
+document.querySelector('#current-0').textContent = '0';
+document.querySelector('#current-1').textContent = '0';
+
+// Roll the dice button
+document.querySelector('.btn-roll').addEventListener('click', () => {
+    diceNumber = Math.floor(Math.random() * 6) + 1;
+    let diceObj = document.getElementById('dice');
+    diceObj.style.display = 'block';
+    diceObj.src = `./images/dice-${diceNumber}.png`; 
+    if (diceNumber !== 1) {
+        roundScore += diceNumber;
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+    } else {
+        roundScore = 0;
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        document.getElementById('current-0').textContent = '0'
+        document.getElementById('current-1').textContent = '0'
+        document.querySelector(`.player-0-panel`).classList.toggle('active');
+        document.querySelector(`.player-1-panel`).classList.toggle('active');
+        document.getElementById('dice').style.display = 'none';
+    }
+});
